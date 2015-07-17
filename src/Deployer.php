5101d97c25325ea3762dba390858470da954f269
Shawn Grover <sgrover@open2space.com>
2015-07-17 03:30:14 -0600

    more logging

--- src/Deployer.php
+++ src/Deployer.php
@@ -141,6 +141,11 @@ class Deployer {
 
 		$url = $this->jenkins .'/buildByToken/build?'. http_build_query($params);
 
+		if ($this->rawdata) {
+			$this->log(' - url: '. $url);
+		}
+
+
 		$ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
